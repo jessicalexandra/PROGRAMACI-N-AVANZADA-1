b02a0a25e4e40287543d6bbad3dacea2c83c5794
yessica <yessica-flores@hotmail.com>
2023-03-10 16:45:08 -0500

    se actualiza

---
+++ punto3flecha.js
@@ -0,0 +1,8 @@
+// 3. PROBLEMA: La luna de Endor pierde temperatura día a día, para ello
+// hay que hacer un monitoreo constante, Cree una función de flecha que
+// permita calcular la temperatura media de la luna a partir de la
+// temperatura máxima y mínima de cada día 
+
+let temperatura=(max,min)=> (max+min)/2
+
+console.log(temperatura(30,40))
