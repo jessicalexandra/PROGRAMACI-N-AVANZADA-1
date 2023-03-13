7a7458ac7524e15e33e2d0ab959bfa5e13e755b9
DUVAN100 <duvanvivas100@gmail.com>
2023-03-12 22:30:43 -0500

    actualiza

---
+++ punto10.js
@@ -0,0 +1,13 @@
+// 10. Grogu ha iniciado su entrenamiento Jedi y debe activar un
+// software que permita clasificar su dieta:
+// alimento = {
+// nombre,
+// tipo (Puede ser vegetal, animal, insecto),
+// nivel de energía aportado (números entre 1 00-500)
+// }
+// Cree un programa que permita recibir 300 alimentos y mediante una
+// función primaria después de 5 segundos se pueda obtener solo los
+// alimentos de tipo vegetal que entreguen más de 200 unidades de
+// energía. Finalmente, una función callback debe permitir entregar la
+// sumatoria de niveles de energía entregados por los alimentos vegetales
+// consumidos en la dieta de Grogu.
---
+++ punto7Tradicional.js
@@ -0,0 +1,21 @@
+//PROBLEMA: se necesita clasificar 10 números enteros positivos
+// o negativos que contienen códigos de naves de guerra. Diseño
+// un algoritmo que permita recibir dichos 10 números e indicar
+// ¿Cuál fue el más cercano a cero? (Si el número es negativo se
+// debe mostrar el resultado del valor absoluto)//
+
+let numeros=[-1,4,6,7,8,8,-9]
+let cerca0=numeros[0]
+if (cerca0<0) {
+    cerca0=Math.abs(cerca0)
+} 
+ numeros.forEach(function(numero){
+    if (numero<0) {
+        numero=Math.abs(numero)
+    }
+
+    if (numero<cerca0) {
+        cerca0=numero
+    }
+ });
+console.log(cerca0)
